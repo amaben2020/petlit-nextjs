13301eec479df15c7e0f8340829bb8bd348eeb12
algomachine007 <amaben202phantom@gmail.com>
2022-10-08 15:31:58 +0100

    building the dynamic page functions with getStaticProps and get StaticPaths

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Blurhash algorithm : https://www.newline.co/@kchan/static-site-generation-with-nextjs-and-typescript-part-iii-optimizing-image-loading-with-plaiceholder-and-blurhash--2e153353
 
 $ npm install --save plaiceholder @plaiceholder/next react-blurhash
+
+https://www.newline.co/@kchan/static-site-generation-with-nextjs-and-typescript-part-iv-dynamic-routes-with-getstaticpaths--a991c4cd
+getStaticPaths: If your Next.js application depends on external data to statically generate pages for dynamic routes, such as from a content management system (CMS) for blog articles, then you can export a getStaticPaths() function that specifies the paths to generate at build time, like so:
+
+Creating several components
+touch components/{AnimalCard,AnimalCardsList}.tsx
---
+++ pages/types/[type].tsx
@@ -0,0 +1,167 @@
+// ...
+
+import { GetStaticProps, NextPage, GetStaticPaths } from "next";
+import { ParsedUrlQuery } from "querystring";
+import {
+  Animal,
+  AnimalType,
+  AnimalTypeBreed,
+  AnimalTypesResponse,
+} from "../../shared/interfaces/petfinder.interface";
+
+export interface TypePageProps {
+  type: AnimalType;
+  adoptedAnimals: Animal[];
+}
+
+const {
+  NEXT_PUBLIC_PETFINDER_API_URL,
+  NEXT_PUBLIC_PETFINDER_CLIENT_ID,
+  NEXT_PUBLIC_PETFINDER_CLIENT_SECRET,
+} = process.env;
+
+const getAccessToken = async () => {
+  const { access_token } = await (
+    await fetch(`${NEXT_PUBLIC_PETFINDER_API_URL}/oauth2/token`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        grant_type: "client_credentials",
+        client_id: NEXT_PUBLIC_PETFINDER_CLIENT_ID,
+        client_secret: NEXT_PUBLIC_PETFINDER_CLIENT_SECRET,
+      }),
+    })
+  ).json();
+
+  return access_token;
+};
+
+interface PageProps {
+  type: AnimalType;
+  adoptedAnimals: Animal[];
+}
+
+interface StaticPathParams extends ParsedUrlQuery {
+  type: string;
+}
+
+export const getStaticProps: GetStaticProps<
+  PageProps,
+  StaticPathParams
+> = async ({ params }) => {
+  let adoptedAnimals: Animal[] = [],
+    breeds: AnimalTypeBreed[] = [],
+    type!: AnimalType;
+
+  let { type: typeParam } = params as StaticPathParams;
+
+  try {
+    const accessToken = await getAccessToken();
+
+    ({ type } = await (
+      await fetch(`${NEXT_PUBLIC_PETFINDER_API_URL}/types/${typeParam}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+    ).json());
+
+    ({ breeds } = await (
+      await fetch(
+        `${NEXT_PUBLIC_PETFINDER_API_URL}/types/${typeParam}/breeds`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+    ).json());
+
+    ({ animals: adoptedAnimals } = await (
+      await fetch(
+        `${NEXT_PUBLIC_PETFINDER_API_URL}/animals?type=${typeParam}&status=adopted&limit=5`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+    ).json());
+  } catch (err) {
+    console.error(err);
+  }
+
+  return {
+    props: {
+      type: {
+        ...(type || {}),
+        id: params.type,
+        breeds,
+      },
+      adoptedAnimals,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  let paths = [];
+
+  try {
+    const accessToken = await getAccessToken();
+
+    const { types }: AnimalTypesResponse = await (
+      await fetch(`${NEXT_PUBLIC_PETFINDER_API_URL}/types`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+    ).json();
+
+    if (types.length > 0) {
+      paths = types.map((type) => ({
+        params: {
+          type: (type._links.self.href.match(/\/types\/([\w-]+)$/) || "")[1],
+        },
+      }));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  return {
+    paths,
+    fallback: false, // Return a 404 page for a non-existent type.
+  };
+};
+
+const TypePage: NextPage<TypePageProps> = () => (
+  <main>
+    <section className="relative mb-7" id="overview">
+      <h1 className="text-7xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+        Lorem Ipsum
+      </h1>
+      <p className="mt-7 text-2xl text-gray-400">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </section>
+    <section className="relative mb-7" id="recently-adopted">
+      <h3 className="text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl mb-7">
+        Recently Adopted
+      </h3>
+    </section>
+  </main>
+);
+
+export default TypePage;
